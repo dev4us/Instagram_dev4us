4fbbc520e15fad97449de7523350ad1654881b46
dev4us <bronxofsoul@gmail.com>
2018-09-15 14:57:40 +0900

    Install Router and Setting
    Install Router and Setting, Install i18n (ready to set Laungage)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux'; 
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from 'redux/configureStore';
+
 import 'index.css';
 import App from 'App';
 
-import { Provider } from 'react-redux'; 
-import store from 'redux/configureStore';
-
-console.log(store.getState());
-
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <ConnectedRouter history={history}>
+            <App />
+        </ConnectedRouter>        
     </Provider>,
     document.getElementById('root'));
 
