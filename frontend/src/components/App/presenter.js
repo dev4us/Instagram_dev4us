121973a3a4ded30380cb5405d1837c094731b31d
dev4us <bronxofsoul@gmail.com>
2018-10-07 16:15:19 +0900

    Create LoginForm, SignupForm
    Create LoginForm, SignupForm

--- frontend/src/components/App/presenter.js
+++ frontend/src/components/App/presenter.js
@@ -1,15 +1,33 @@
-import React, { Component } from 'react';
-import styles from './styles.scss';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Switch } from 'react-router-dom';
+import './styles.scss';
 import Footer from 'components/Footer';
+import Auth from 'components/Auth';
 
-class App extends Component {
-  render() {
-    return (
-      <div className={styles.App}>
-        <Footer></Footer>
-      </div>
-    );
-  }
+const App = props => [
+  // Nav
+  // Routes
+  props.inLoggedIn ? <PrivateRoutes key={2}/> : <PublicRoutes key={2}/>,
+  <Footer key={3}/>
+]
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired
 }
+
+const PrivateRoutes = props => (
+  <Switch>
+    <Route exact path="/" render={() => "feed"}/>
+    <Route path="/explore" render={() => "explore"}/>
+  </Switch>
+)
+
+const PublicRoutes = props => (
+  <Switch>
+    <Route exact path="/" component={Auth}/>
+    <Route path="/forgot" render={() => "password"}/>
+  </Switch>
+)
 
 export default App;
